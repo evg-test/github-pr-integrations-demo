51defd90f20507cc241f260260331c0ec5c3f2f7
Suraj Shrestha <sshrestha@salesforc.com>
2023-07-11 11:01:44 -0400

    java spring hello world

---
+++ src/main/java/com/evergage/githubprdemo/Greeting.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2010-2023 Salesforce, Inc.
+ * All rights reserved.
+ */
+
+package com.evergage.githubprdemo;
+
+public class Greeting {
+    public long id;
+    public String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+}
---
+++ src/test/java/com/evergage/githubprdemo/GithubPrDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evergage.githubprdemo;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+class GithubPrDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assert.assertEquals("test", "test");
+    }
+
+}
